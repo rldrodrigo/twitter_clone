3c416c61ae19244a6166717b741ea124d6b7a0a0
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-10-08 17:34:10 -0300

    Autenticação de usuário e inicio da time line

--- App/Models/Usuario.php
+++ App/Models/Usuario.php
@@ -62,4 +62,23 @@ class Usuario extends Model
 
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
+
+
+    public function autenticar()
+    {
+        $query = "SELECT id, nome, email FROM usuarios WHERE email = :email AND senha = :senha";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':email', $this->__get('email'));
+        $stmt->bindValue(':senha', $this->__get('senha'));
+        $stmt->execute();
+
+        $usuario = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        if (!empty($usuario['id']) && !empty($usuario['nome'])) {
+            $this->__set('id', $usuario['id']);
+            $this->__set('nome', $usuario['nome']);
+        }
+
+        return $this;
+    }
 }
