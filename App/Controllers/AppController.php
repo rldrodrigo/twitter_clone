6f6d6b86dd5749d9fc65f112a9697b4f6af00685
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-10-08 19:44:53 -0300

    Pesquisar por outros usuário

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -48,4 +48,23 @@ class AppController extends Action
             header('Location:/?login=erro');
         }
     }
+
+    public function quemSeguir()
+    {
+        $this->validaAutenticacao();
+
+        $pesquisarPor = isset($_GET['pesquisarPor']) ? $_GET['pesquisarPor'] : '';
+
+        $usuarios = array();
+
+        if ($pesquisarPor != '') {
+            $usuario = Container::getModel('Usuario');
+            $usuario->__set('nome', $pesquisarPor);
+            $usuarios = $usuario->getAll();
+        }
+
+        $this->view->usuarios = $usuarios;
+
+        $this->render('quemSeguir');
+    }
 }
