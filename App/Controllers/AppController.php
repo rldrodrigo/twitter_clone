aedebfd0ff7b85c16674fecbed99cfcb15b9b474
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-10-09 13:01:56 -0300

    Contador de seguidores e tweets
    Foi adicionado contadores de tweet e seguidores no perfil do usuário logado

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -22,6 +22,15 @@ class AppController extends Action
 
         $this->view->tweets = $tweets;
 
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id', $_SESSION['id']);
+
+        $this->view->info_usuario = $usuario->getInfoUsuario();
+        $this->view->total_tweets = $usuario->getTotalTweets();
+        $this->view->total_seguindo = $usuario->getTotalSeguindo();
+        $this->view->total_seguidores = $usuario->getTotalSeguidores();
+
+
         $this->render('timeline');
     }
 
@@ -65,6 +74,14 @@ class AppController extends Action
         }
 
         $this->view->usuarios = $usuarios;
+
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id', $_SESSION['id']);
+
+        $this->view->info_usuario = $usuario->getInfoUsuario();
+        $this->view->total_tweets = $usuario->getTotalTweets();
+        $this->view->total_seguindo = $usuario->getTotalSeguindo();
+        $this->view->total_seguidores = $usuario->getTotalSeguidores();
 
         $this->render('quemSeguir');
     }
